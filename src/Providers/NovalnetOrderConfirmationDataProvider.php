dc623a4a3477a318e0d2d7aeb0eb921db2b7cb74
kotteeswaranv <kotteeswaran@rootsbridge.com>
2018-04-25 19:59:43 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -50,7 +50,7 @@ class NovalnetOrderConfirmationDataProvider
 
         foreach($properties as $property)
         {
-            $paymentHelper->testLogTest('CHECKOBJ',$property);                 
+            $paymentHelper->testLogTest('CHECKOBJ',is_object($property));                 
             $paymentHelper->testLogTest('CHECKOBJVAL',$property->value);                
             $paymentHelper->testLogTest('CHECKOBJTYPE',$property->typeId);
             //if($property->typeId == '3' && $property->value == $paymentMethodId)
